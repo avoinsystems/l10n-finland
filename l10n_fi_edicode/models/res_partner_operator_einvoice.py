9d53f934dc8f828b9b56f390b6cc4ab8b6020ac8
Daniels Andersons <daniels.andersons@avoin.systems>
2018-12-28 16:21:27 +0200

    Added modules

---
+++ l10n_fi_edicode/models/res_partner_operator_einvoice.py
@@ -0,0 +1,14 @@
+from odoo import fields, models
+
+
+class ResPartnerOperatorEinvoice(models.Model):
+    _name = 'res.partner.operator.einvoice'
+
+    name = fields.Char(
+        string='Operator',
+        required=True,
+    )
+    identifier = fields.Char(
+        string='Identifier',
+        required=True,
+    )
