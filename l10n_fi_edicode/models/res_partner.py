ab7fe471f633fbea90e11a917f2b7a3d1b1a338a
Tatiana Deribina <t.i.deribina@gmail.com>
2019-11-21 15:45:33 +0200

    [MIG] l10n_fi_edicode: Migration to 13.0
    - Upgrade structure of module.
    - Change `einvoice_operator` field name to `einvoice_operator_id`
    - Change `name_get` for `res.partner.operator.einvoice` to
    remove redundant field `einvoice_operator_identifier` from
    `res.partner` model

--- l10n_fi_edicode/models/res_partner.py
+++ l10n_fi_edicode/models/res_partner.py
@@ -5,12 +5,8 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     edicode = fields.Char(string="Edicode")
-    einvoice_operator = fields.Many2one(
+    einvoice_operator_id = fields.Many2one(
         comodel_name="res.partner.operator.einvoice",
         string="eInvoice Operator",
-    )
-    einvoice_operator_identifier = fields.Char(
-        string="Operator ID",
-        related="einvoice_operator.identifier",
-        readonly=True,
+        old_name="einvoice_operator",
     )
